65a167807fe497fa8283a735c2feca718a88da6d
Prakash R <git@prakashrx.com>
2023-02-20 13:42:25 +0800

    feat: update test

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen bg-circuit">
-  <h1 class="text-5xl">Git Live Server</h1>
+  <h1 class="text-5xl">Git Live Server!</h1>
   <div class="mt-2 text-center">
     <p class="text-indigo-900">This is a sample Git live server demo page</p>
     <a href="https://github.com/prakashrx/git-live-server/" target="_blank" class="text-sm  text-indigo-900-500">prakashrx/git-live-server</a>
